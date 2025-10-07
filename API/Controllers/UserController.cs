79a02e6de9926ddce4bc806bbb2d139a657e72eb
Josef Kuchař <xkuchar@fi.muni.cz>
2025-10-07 19:45:49 +0200

    Switch from minimal api to controllers

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+using Business.DTOs;
+using Business.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly ILogger<UserController> _logger;
+    private readonly IUserService _userService;
+
+    public UserController(ILogger<UserController> logger, IUserService userService)
+    {
+        _logger = logger;
+        _userService = userService;
+    }
+
+    [HttpGet(Name = "GetUsers")]
+    public async Task<IEnumerable<UserDto>> Get()
+    {
+        return await _userService.GetAllAsync();
+    }
+}
