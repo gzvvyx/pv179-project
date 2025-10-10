9b00d376f88318186cea95823705ecd8d5c760cc
Andrej Nešpor <xnespor@fi.muni.cz>
2025-10-10 17:07:15 +0200

    feat: add subscription entity

--- DAL/Data/AppDbContext.cs
+++ DAL/Data/AppDbContext.cs
@@ -8,6 +8,7 @@ public class AppDbContext : IdentityDbContext
 {
     public DbSet<User> Users { get; set; }
     public DbSet<Order> Orders { get; set; }
+    public DbSet<Subscription> Subscriptions { get; set; }
     public DbSet<Video> Videos { get; set; }
     public DbSet<Playlist> Playlists { get; set; }
     public DbSet<Comment> Comments { get; set; }
---
+++ DAL/Models/Subscription.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+public enum SubscriptionTimeframe
+{
+    Month,
+    HalfYear,
+    Year
+}
+
+
+namespace DAL.Models
+{
+    public class Subscription
+    {
+        public required int Id { get; set; }
+        [ForeignKey(nameof(Orderer))]
+        public required string OrdererId { get; set; }
+        [ForeignKey(nameof(Creator))]
+        public required string CreatorId { get; set; }
+        public required bool Active { get; set; }
+        public required SubscriptionTimeframe Timeframe { get; set; }
+        public required DateTime SubscribedAt { get; set; }
+        public required DateTime LastRenewedAt { get; set; }
+        public required DateTime ExpiresAt { get; set; }
+
+        public required User Orderer { get; set; }
+        public required User Creator { get; set; }
+    }
+}
