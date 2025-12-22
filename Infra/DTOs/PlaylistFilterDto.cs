3146eab2338990806ec0fcaed5d5253282a32213
JiriStipek <567776@muni.cz>
2025-12-22 18:39:24 +0100

    feat: add new filter DTOs

---
+++ Infra/DTOs/PlaylistFilterDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Infra.DTOs
+{
+    public class PlaylistFilterDto
+    {
+        public string? Name { get; set; }
+        public string? CreatorId { get; set; }
+        public string? Description { get; set; }
+
+
+    }
+}
---
+++ Infra/DTOs/UserFilterDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Infra.DTOs
+{
+    public class UserFilterDto
+    {
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+    }
+}
